9b903f2f96f485e2211b77bde4fd9ae1795f23dc
anthuand <anthuandamas@gmail.com>
2021-08-18 00:54:57 -0400

    Cambiando a lower palabra clave

--- pr.py
+++
@@ -1,5 +0,0 @@
-import unicodedata
-
-cadena =" Vendo Dólares a 60 cup"
-cadena_normalize=unicodedata.normalize('NFKD', cadena).encode('ASCII', 'ignore').lower()
-print(str(cadena_normalize).find("dolares"))
