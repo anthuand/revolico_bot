a97a88c9cc3faf8473012a04f803758f7f350cd9
Anthuan <anthuandamas@gmail.com>
2022-03-01 07:01:00 -0500

    Anadiendo banderas a la hora de enviar el mensaje a telegram

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ from sqlite3 import Error
 def sql_connection():
     try:
         conexion = sqlite3.connect('anuncios.db')
+        print("Conectado a la Db")
         return conexion
     except Error:
         print(Error)
@@ -18,6 +19,7 @@ def crear_tabla_filtros():
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS filtros(id integer PRIMARY KEY, departamento text,palabra_clave text, precio_min integer, precio_max integer, provincia text, municipio text, fotos text)")
         conexion.commit()
+        print("Creada tabla de filtros")
         conexion.close()
     except Exception as Error:
         print(Error)
@@ -30,7 +32,7 @@ def insertar_filtro(departamento,palabra_clave, precio_min=None, precio_max=None
         cursor.execute(
             'INSERT INTO filtros( departamento,palabra_clave, precio_min, precio_max, provincia, municipio, fotos) VALUES( ?,?, ?, ?, ?, ?,?)',
             (departamento, palabra_clave, precio_min, precio_max, provincia, municipio, fotos))
-
+        print("Filtro insertado en la DB")
         conexion.commit()
     except Exception as e:
         print(e)
@@ -42,6 +44,7 @@ def obtener_filtros():
         cursor = conexion.cursor()
         cursor = conexion.execute("SELECT * from filtros")
         filtros = cursor.fetchall()
+        print("Obtuve los filtros de la DB")
         conexion.close()
         return filtros
     except Exception as e:
@@ -55,7 +58,7 @@ def actualizar_filtro(id, param, value):
         query = 'UPDATE filtros set ' + str(param) + ' = ' + str(value) + ' where id = ' + str(id)
         cursor = conexion.execute(query)
         conexion.commit()
-
+        print("Filtro actualizado")
         conexion.close()
     except Exception as e:
         print(e)
@@ -68,7 +71,7 @@ def eliminar_filtro(id):
         cursor = conexion.cursor()
         cursor = conexion.execute("DELETE from filtros where id = ?;", id)
         conexion.commit()
-
+        print('Se elimino un filtro')
         conexion.close()
 
     except Exception as e:
@@ -83,7 +86,7 @@ def eliminar_todos_los_filtros():
         cursor.execute("DROP TABLE IF EXISTS filtros")
         crear_tabla_filtros()
         conexion.commit()
-
+        print("Todos los filtros eliminados")
         conexion.close()
 
     except Exception as e:
@@ -99,6 +102,7 @@ def crear_tabla_anuncio():
         cursor.execute(
             "CREATE TABLE IF NOT EXISTS anuncios(id integer PRIMARY KEY, url text, titulo text, precio text, descripcion text, fecha text, ubicacion text,foto text)")
         conexion.commit()
+        print("creada tabla anuncio en la db")
         conexion.close()
     except Exception as Error:
         print(Error)
@@ -111,7 +115,7 @@ def insertar_anuncio(url, titulo, precio, descripcion, fecha, ubicacion, foto):
         cursor.execute(
             'INSERT INTO anuncios( url, titulo, precio, descripcion, fecha, ubicacion, foto) VALUES( ?, ?, ?, ?, ?,?,?)',
             (url, titulo, precio, descripcion, fecha, ubicacion, foto))
-
+        print("Se inserto anuncio en la db")
         conexion.commit()
     except Exception as e:
         print(e)
@@ -123,6 +127,7 @@ def obtener_anuncios():
         cursor = conexion.cursor()
         cursor = conexion.execute("SELECT * from anuncios")
         anuncios = cursor.fetchall()
+        print("Obtuve anuncios de la DB")
         conexion.close()
         return anuncios
     except Exception as e:
